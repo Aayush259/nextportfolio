d2a8781477d1c4167a7b04453bd0fe82d3aa8ba5
Square <aayushk4980@gmail.com>
2024-09-05 22:47:56 +0530

    Added Github link

---
+++ src/app/globalConstants.ts
@@ -0,0 +1,10 @@
+
+
+const myInfo = {
+    id: 1,
+    name: "Aayush",
+    designation: "Frontend Developer",
+    image: "https://avatars.githubusercontent.com/u/159794430?v=4",
+};
+
+export { myInfo };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+import { AnimatedTooltip } from "@/components/ui/AnimatedTooltip";
+import { myInfo } from "./globalConstants";
+import Link from "next/link";
 
 export default function Home() {
+
   return (
     <div>
-      Next JS Portfolio
+      <Link
+        href={"https://github.com/Aayush259"}
+        target="_blank"
+        className="fixed right-20 bottom-10 z-50">
+        <AnimatedTooltip items={[myInfo]} />
+      </Link>
     </div>
   );
 };
