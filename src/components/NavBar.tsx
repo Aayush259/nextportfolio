4d2c11f49e39efe8a11dc50d2f2661dff738325d
Square <aayushk4980@gmail.com>
2024-09-06 15:31:46 +0530

    Added X Link

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -45,7 +45,7 @@ export function NavBar() {
       icon: (
         <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
       ),
-      href: "/",
+      href: "https://x.com/Aayush259_",
     },
     {
       title: "GitHub",
